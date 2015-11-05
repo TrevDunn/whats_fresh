e2f663122dd34abf269398011db60d7a6b1ea41f
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-11-05 16:24:49 -0500

    added basic session functionality

--- whats_fresh_app/app/models/user.rb
+++ whats_fresh_app/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :email, presence: true
 	validates :password, length: { in: 6..20 }
-	validates :paswwrod, presence: true
+	validates :password, presence: true
 end
