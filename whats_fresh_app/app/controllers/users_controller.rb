a8cbdca0679fbc1cafdfdc72897ed2ea15db056c
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-11-05 13:49:42 -0500

    aded create_users migration

--- whats_fresh_app/app/controllers/users_controller.rb
+++ whats_fresh_app/app/controllers/users_controller.rb
@@ -3,18 +3,29 @@ class UsersController < ApplicationController::Base
 		@users = Users.all
 	end
 	def show
-		@user = 
+		@user = User.find(params[:id])
 	end
 	def new
-
+		@user = User.new
 	end
 	def edit
-
+		@user = User.find(params[:id])
 	end
 	def create
-
+		user = User.new(user_params)
+		if user.save
+			redirect_to ('/')
+		else
+			redirect_to ('/users/new')
+		end
 	end
 	def update
+		id = param[:id]
+		user = User.find(id)
+		if user.update(user_params)
+			redirect_to ('/user')
+		else
+			redirect_to 
 
 	end
 	def destroy
